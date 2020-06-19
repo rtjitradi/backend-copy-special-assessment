304179f478c4daf008c0b5de55cd3f3218ae8f23
Reginald Tjitradi <rtjitradi@gmail.com>
2020-06-19 13:59:12 -0700

    Thanks for the guidance Stew and Daniel for having the study hall recorded

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Reggy Tjitradi guided by Stew with Daniel's study hall session"
 
 import re
 import os
@@ -19,8 +19,18 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    #Get list of files in directory
+    paths = os.listdir(dirname)
+
+    #identify "special" files -- "__w__"
+    special_paths = []
+    for filename in paths:
+        match = re.search(r"__\w+__", filename)
+        if match:
+            special_paths.append(os.path.abspath(os.path.join(dirname, filename)))     
+    #print(special_paths)
+    #return absolute paths to special files
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
@@ -29,9 +39,13 @@ def copy_to(path_list, dest_dir):
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
-    return
-
+    """zip special files"""
+    cmd = ["zip", "-j", dest_zip]
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    print(" ".join(cmd))
+    subprocess.check_output(cmd)
+    
 
 def main(args):
     """Main driver code for copyspecial."""
@@ -39,8 +53,12 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument("from_dir", help="dest zipfile for special")
+    
     ns = parser.parse_args(args)
+
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
@@ -52,6 +70,12 @@ def main(args):
 
     # Your code here: Invoke (call) your functions
 
+    special_paths = get_special_paths(ns.from_dir)
+
+    if ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        print("\n".join(special_paths))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
